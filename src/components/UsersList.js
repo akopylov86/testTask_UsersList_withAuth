206b14cade84bb11a0f62be3feb80ce093253a88
akopylov86 <copylove@ukr.net>
2020-11-18 14:34:46 +0200

    MOve sort function inside render

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -4,44 +4,24 @@ import '../index.css';
 import {connect} from "react-redux";
 import {ASC} from "../constatns/sortTypes";
 
-const UsersList = ({usersListData, userListError}) => {
+const UsersList = ({usersListData, usersListError, usersListFilter, usersListSort}) => {
 
-    if(userListError) {
+    if(usersListError) {
         return (<div>oops, something went wrong</div>);
     }
 
-    const dataIsEmpty = usersListData.length === 0;
-    return(
-        <div>
-            {dataIsEmpty
-                ? <div className="table">
-                    <UserLine />
-                  </div>
-                : <div className="table">
-                    {usersListData.map( user => (<UserLine key={user.email} user={user} />) ) }
-                  </div>
-            }
-        </div>
-    )
-
-}
-
-export default connect(
-    state=>({
-        userListError: state.usersList.error,
-        usersListData: state.usersList.data
-            .filter(
-                user=>(user.name.first
-                    .toUpperCase()
-                    .includes(state.usersList.filter.toUpperCase()))
-                || (user.name.last
-                        .toUpperCase()
-                        .includes(state.usersList.filter.toUpperCase()))
-                )
-            .sort((a,b)=>{
+    const usersListFiltered = usersListData.filter(
+        user=>(user.name.first
+                .toUpperCase()
+                .includes(usersListFilter.toUpperCase()))
+            || (user.name.last
+                .toUpperCase()
+                .includes(usersListFilter.toUpperCase()))
+        )
+        .sort((a,b)=>{
                 let valueA;
                 let valueB;
-                const [field1,field2] = state.usersList.sort.field.split(".")
+                const [field1,field2] = usersListSort.field.split(".")
                 if (field2){
                     valueA = a[field1][field2];
                     valueB = b[field1][field2];
@@ -49,8 +29,8 @@ export default connect(
                     valueA = a[field1];
                     valueB = b[field1];
                 }
-                const sortLeft = state.usersList.sort.direction === ASC ? 1 : -1;
-                const sortRight = state.usersList.sort.direction === ASC ? -1 : 1;
+                const sortLeft = usersListSort.direction === ASC ? 1 : -1;
+                const sortRight = usersListSort.direction === ASC ? -1 : 1;
 
                 if(!isNaN(valueA)&&!isNaN(valueB)){
                     return (Number(valueA) > Number(valueB)
@@ -62,6 +42,29 @@ export default connect(
                         : sortRight)
                 }
             }
-            ),
+        )
+
+    // const dataIsEmpty = usersListFiltered.length === 0;
+    return(
+        <div>
+            {usersListFiltered.length === 0
+                ? <div className="table">
+                    <UserLine />
+                  </div>
+                : <div className="table">
+                    {usersListFiltered.map( user => (<UserLine key={user.email} user={user} />) ) }
+                  </div>
+            }
+        </div>
+    )
+
+}
+
+export default connect(
+    state=>({
+        usersListError: state.usersList.error,
+        usersListData: state.usersList.data,
+        usersListFilter: state.usersList.filter,
+        usersListSort: state.usersList.sort,
     })
 )(UsersList);
